0ae6f9f9f70fd9fb10e275c89ff37ce68e4829a5
Sir1527 <pengcheng.tang@outlook.com>
2024-04-04 09:47:37 +0800

    Remove vue

--- vue/src/apis/news.ts
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/http";
-
-
-export const getNewsTopListAPI = () => {
-    return request({
-        url: 'news/selectAll',
-        method: 'GET',
-    })
-}
--- vue/src/utils/http.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const httpInstance = axios.create({
-    baseURL:'http://localhost:9090',
-    timeout: 5000,
-})
-
-export default httpInstance;
